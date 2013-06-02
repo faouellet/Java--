b84d858e1e025575a24d4b9ee9f2cdb0b9278f9c
Felix <felix@ubuntu.(none)>
2013-06-02 17:49:11 -0400

    Updating CodeGen

--- codegen.cpp
+++ codegen.cpp
@@ -1,9 +1,15 @@
 #include "codegen.h"
 
-CodeGen::CodeGen()
+#include "codegen.h"
+#include "parser.hpp"
+
+void CodeGenContext::GenerateBlock(NBlock & in_Block)
 {
+	std::cout << "Genrating code..." << std::endl;
+
+	std::vector<const Type*> l_ArgTypes;
+	FunctionType* l_FType = FunctionType::get(Type::getVoidTy(getGlobalContext()), l_ArgTypes, false);
+	m_MainFunction = Function::Create(l_FType, GlobalValue::InternalLinkage)
 }
 
-CodeGen::~CodeGen()
-{
-}
+
--- codegen.h
+++ codegen.h
@@ -9,6 +9,7 @@
 #include <llvm/Instructions.h>
 #include <llvm/LLVMContext.h>
 #include <llvm/Module.h>
+#include <llvm/ModuleProvider.h>
 #include <llvm/PassManager.h>
 #include <llvm/Type.h>
 
@@ -18,13 +19,40 @@
 
 #include <llvm/Bitcode/ReaderWriter.h>
 
-#include <llvm/Support/
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/JIT.h>
 
-class CodeGen
+#include <llvm/Support/IRBuilder.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include <llvm/Target/TargetSelect.h>
+
+struct CodeGenBlock
 {
+	BasicBlock* Block;
+	std::map<Std::string, Value*> Locals;
+};
+
+class CodeGenContext
+{
+private:
+	std::stack<CodeGenBlock*> m_Blocks;
+	Function* m_MainFunction;
+
 public:
-	CodeGen();
-	~CodeGen();
+	// TODO : Ewww...
+	Module* Mod;
+
+public:
+	CodeGenContext() { Mod = new Module(:"main", getGlobalContext()); }
+	~CodeGenContext();
+
+	void GenerateCode(NBlock & in_Root);
+	GenericValue RunCode();
+	std::map<std::string, Value*>& Locals() { return m_Blocks.top()->Locals; } 
+	BasicBlock* CurrentBlock() { return m_Blocks.top()->Block; }
+	void PushBlock(BasicBlock * in_Block) { m_Blocks.push(new CodeGenBlock()); m_Blocks.top()->Block = in_Block; }
+	void PopBlock() { CodeGenBlock* l_Top = m_Blocks.top(); m_Blocks.pop(); delete l_Top; }
 };
 
 #endif // CODEGEN_H
